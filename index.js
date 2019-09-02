ccbf2ca86db8d49d0fd439bb7c4bb161e42128af
Viktor Marchenko <viktor.marchenko@gmail.com>
2019-09-02 18:23:14 +0300

    Proxy in progress

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const http = require('https');
-
-module.exports = (res, req) => {
+module.exports = (req, res) => {
   const options = {
-    ...req,
+    path: require('url').parse(req.url, true).path,
+    method: req.method,
     hostname: 'api.telegram.org',
   };
-  const apiRequest = http.request(options, apiResponse => {
-    apiResponse.pipe(clientResponse, {
+  const apiRequest = require('https').request(options, apiResponse => {
+    apiResponse.pipe(res, {
       end: true
     });
   });
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString(); // convert Buffer to string
+    });
+    req.on('end', () => {
+      apiRequest.write(body);
+      apiRequest.end();
+    });
+  } else {
+    apiRequest.end();
+  }
+  // req.pipe(apiRequest, { end: true });
 }
